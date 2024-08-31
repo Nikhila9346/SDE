69cd1f7b75e699d31f2b339db5ca60fffaebaf97
Nikhils9346 <nikhilabeereddy@gmail.com>
2024-08-31 23:37:20 +0530

    MaxSubarrayProduct

--- MaxSubarrayProduct.py
+++ MaxSubarrayProduct.py
@@ -5,17 +5,19 @@ def maxProduct(nums):
     prefix = 1
     postfix = 1
 
-    for i in range(len(nums)-1):
+    for i in range(len(nums)):
         prefix = 1 if prefix == 0 else prefix
         postfix = 1 if postfix == 0 else postfix
 
         prefix *= nums[i]
+        # print(prefix)
         postfix *= nums[len(nums)-i-1]
-
+        # print(postfix)
         max_prod = max(max_prod, max(prefix, postfix))
     
     return max_prod
 
 
 nums = [2,3,-2,4]
+nums = [-4, -3]
 print(maxProduct(nums))   #6
