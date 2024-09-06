301a838f1816cf9025c3021d97545e37a5833ff9
Nikhils9346 <nikhilabeereddy@gmail.com>
2024-09-06 19:55:10 +0530

    No Duplicates

--- EPAM/noDuplicates.py
+++ EPAM/noDuplicates.py
@@ -1,13 +1,21 @@
 '''
 Return the string without duplicates (case-insensitive) and the order should be preserved
 '''
+
+def case(k):
+    if 65<=ord(k)<=90:
+        l = ord(k) + 32
+    else:
+        l = ord(k) - 32
+    return chr(l)
+
 def noDuplicates(nums):
     i = 0
     s = nums[0]
     for j in range(1, len(nums)):
         dif = ord(nums[i]) - ord(nums[j])
         if dif not in [32, -32, 0]:
-            if (nums[j].upper() not in s) and (nums[j].lower() not in s):
+            if (nums[j] not in s) and (case(nums[j]) not in s):
                 s += nums[j]
                 i = j
     return s
