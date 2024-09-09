cc37e49f51e4dcc0a91859c6dd33dc8a0573b1e2
Nikhils9346 <nikhilabeereddy@gmail.com>
2024-09-09 19:07:51 +0530

    Pascal's Triangle

--- Pascals Triangle/rowcols.py
+++ Pascals Triangle/rowcols.py
@@ -1,8 +1,13 @@
 '''
 Given r and c value, find the value in pascals triangle
 
-To calculate the value at some row and column, we use formula Ncr = n!/r! * (n-r)! which is a brute force approach
+To calculate the value at some row and column, we use formula (N-1)c(r-1) = n!/r! * (n-r)! which is a brute force approach
 Can reduce the time complexity, by formula = n*(n-1)..(r times)/r!
+
+where n --> no.of rows
+      c --> no.of cols
+
+Time Complexity: O(c)
 '''
 
 def pascals(r, c):
