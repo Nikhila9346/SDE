083cefc0269e38e2ba604095d7a1e62c89446ce8
Nikhils9346 <nikhilabeereddy@gmail.com>
2024-09-12 19:59:21 +0530

    Array Leaders

--- ArrayLeaders.py
+++ ArrayLeaders.py
@@ -1,25 +1,21 @@
 '''
+Input: arr = [4, 7, 1, 0]
+Output: 7 1 0
+Explanation: Rightmost element is always a leader. 7 and 1 are greater than the elements in their right side.
 
+Time Complexity: O(n)
+Space Complexity: O(n)
 '''
 
 def arrayLeaders(n, nums):
-    maxi = 0
+    maxi = float('-inf')
     l = []
-    i = 1
-    while i < n:
-        if nums[maxi] > nums[i]:
-            l.append(nums[maxi])
-            i += 1
-            if nums[i] > nums[i+1]:
-                i += 1
-            else:
-                maxi = i + 1
-                i += 2
-        else:
-            maxi = i
-            i += 1
-    l.append(nums[n-1])
-    return l
+
+    for i in range(n-1, -1, -1):
+        if nums[i] > maxi:
+            l.append(nums[i])
+            maxi = nums[i]
+    return l[::-1]
 
 nums = [16,17,4,3,5,2]
 print(arrayLeaders(len(nums), nums))
